237606ef9a7c51ba407b9947b9d30b9ee279eb5c
Kuldeep Chaudhary <kuldeepbits24@gmail.com>
2025-12-23 13:35:26 +0530

    create new java file of addition

---
+++ addition.java
@@ -0,0 +1,8 @@
+public class addition { 
+    public static void main(String[] args) { 
+        int a = 5;
+        int b = 6;
+        int sum = a+b;
+        System.out.println("The addition of both numbers is: " + sum); 
+    }
+}
